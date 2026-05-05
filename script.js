86e0b1e1a753d978d84d55ead142941a3cf29b34
adrianorm20 <adrianorm16@gmail.com>
2026-05-05 16:49:45 -0300

    Fase inicial 1.0

---
+++ script.js
@@ -0,0 +1,29 @@
+function Cadastro() {
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+    localStorage.setItem("usuario", usuario);
+    localStorage.setItem("senha", senha);
+
+    if (usuario === usuario && senha === senha) {
+        window.location.href = "index.html";
+    } else {
+        alert("Cadastro inválido");
+    }
+
+
+}
+
+function login() {
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+    let user = localStorage.getItem("usuario");
+    let pass = localStorage.getItem("senha");
+
+    if (usuario === user && senha === pass) {
+        window.location.href = "index.html";
+    } else {
+        alert("Login inválido");
+    }
+}
